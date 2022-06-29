79bc6be3ded1a920cded0a04ee3f82928fce2c65
kauan <kauanguilhermep12@gmail.com>
2022-06-29 16:14:49 -0300

    k

---
+++ listaDeAlunos.js
@@ -0,0 +1,55 @@
+// let numeroDeAlunos = 10;
+let listaDeAlunos = ["Jonhatan","Josuke","Joseph","Jotaro"]
+//array -> coleção 
+
+
+for (let nome of listaDeAlunos) {
+    console.log("Nome do aluno é " + nome + " no EAD Senai")
+}
+
+for (let nome of listaDeAlunos) {
+    console.log(`Nome do aluno é ${nome} no EAD Senai`)
+    //interpolador ++
+}
+
+
+
+
+
+
+
+
+// for (let contador = 0; contador < numeroDeAlunos; contador++) {
+//     console.log(contador);
+ 
+
+//   if (contador == 0) {
+//    console.log ("Numero igual a zero")
+// }
+//  else if (contador % 2 == 0){
+// console.log("número par")
+// }
+//  else{
+//    console.log ("numero impar")
+// }
+// }
+
+// -------------- 
+
+// let contador = 0
+
+// while (contador < numeroDeAlunos) {
+
+
+//     if (contador == 0) {
+//         console.log ("Numero igual a zero")
+//     }
+//       else if (contador % 2 == 0){
+//         console.log("número par")
+//     }
+//       else{
+//         console.log ("numero impar")
+//     }
+//      contador++
+    
+// }
